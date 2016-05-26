95b99506d7b20d8fec3e3d14ba864b11e5ce1b76
vprusa <vprusa@redhat.com>
2016-05-26 19:45:17 +0200

    Changed URL to test MIQ instance URL

--- test-for-job.py
+++ test-for-job.py
@@ -5,7 +5,7 @@ display = Display(visible=0, size=(800, 600))
 display.start()
  
 browser = webdriver.Firefox()
-browser.get('http://www.google.com')
+browser.get('https://vprusa-docker-2.bc.jonqe.lab.eng.bos.redhat.com')
 print browser.title
 browser.quit()
  
